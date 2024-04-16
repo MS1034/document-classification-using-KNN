4ea253c8933ff9cb11b0bd0b997ee308f4685c52
Subhan Suleman <sbse1034@gmail.com>
2024-04-16 22:31:27 +0500

    Add Database

---
+++ Scrapping/database.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pymongo
+
+
+class Database:
+    def __init__(self, db_name, collection_name, uri):
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+
+    def insert_data(self, data):
+        self.collection.insert_one(data)
